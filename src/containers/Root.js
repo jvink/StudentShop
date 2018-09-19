c2d0e92fbb4d12d711e7f7d2b1c79bbb9b9ecbea
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2018-09-19 23:32:01 +0200

    Implemented Routing!

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import userActionCreator from '../store/actionCreators/user';
+import { Switch, Route } from 'react-router-dom';
+import Home from '../components/Home';
+import NotFound from '../components/NotFound';
 
 class Root extends Component {
-    constructor(props) {
-        super(props);
-
-        this.addUserdata = this.addUserdata.bind(this);
-        this.getUserdata = this.getUserdata.bind(this);
-    }
-
     render() {
       return (
-        <div>
-            <p>Hi!</p>
-            <button type="submit" onClick={() => { this.addUserdata('test') }}>addem</button>
-            <button type="submit" onClick={() => { this.getUserdata() }}>getem</button>
-        </div>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/' component={NotFound} />
+        </Switch>
       );
-    }
-
-    addUserdata(test) {
-        this.props.userActions.getUser(test);
-    }
-
-    getUserdata() {
-        console.log(this.props.userStore.registerUserResult);
     }
 }
 
-export default connect(
-    state => ({
-        userStore: state.user
-    }),
-    dispatch => ({
-        userActions: userActionCreator(dispatch)
-    })
-)(Root);
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Root from './containers/Root';
@@ -9,8 +10,10 @@ import './styles/index.css';
 const store = createStore(rootReducer);
 
 render(
-    <Provider store={store}>
-        <Root/>
-    </Provider>,
+    <BrowserRouter>
+        <Provider store={store}>
+            <Root/>
+        </Provider>
+    </BrowserRouter>,
     document.getElementById('root')
 );
