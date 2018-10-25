7459236b20ad47e7d01d3f05f99f1810eed91fc1
Jurian Vink <0950140@hr.nl>
2018-10-25 11:27:43 +0200

    Added stars

--- src/components/DetailProduct/index.js
+++ src/components/DetailProduct/index.js
@@ -17,29 +17,33 @@ class DetailProduct extends Component {
     }
 
     componentWillMount() {
-        this.setState({data: json});
+        const foundProduct = json.find(x => (x.id === Number(this.props.match.params.id)));
+        this.setState({
+            product: foundProduct,
+            rating: foundProduct.rating
+        });
     }
 
-    changeRating( newRating, name ) {
+    changeRating( newRating ) {
         this.setState({
           rating: newRating
         });
     }
 
     render() {
-        const currentProduct = this.state.data.find(x => (x.id === Number(this.props.match.params.id)));
+        const { product } = this.state;
         return (
             <div>
                 <div className="wrapperDetailProductUpperInfo">
-                    <img className="detailProductImage" alt={currentProduct.name} src={currentProduct.imgUrl}/>
+                    <img className="detailProductImage" alt={product.name} src={product.imgUrl}/>
                     <div className="detailProductInfo">
                         <div className="productTitle">
-                            {currentProduct.name}
+                            {product.name}
                         </div>
                         <div className="productInfoDivider"/>
                         <div className="productUpperInfoUpperWrapper">
                             <div className="productPrice">
-                                {currentProduct.price}
+                                €{product.price}
                             </div>
                             <div style={{textAlign: 'right', width: '100%'}}>
                                 <StarRatings
@@ -61,7 +65,7 @@ class DetailProduct extends Component {
                 </div>
                 <div className="wrapperDetailProductLowerInfo">
                     <div className="productDescription">
-                        Een A4 groot formaat agenda met een zachte laag over de harde kaft. Heeft een leeslint, een elastieken band. Een back to school agenda met een gebonden rug en 144 blaadjes. De taal is in het Nederlands. De inhoud is 7 dagen per 2 pagina,s. De schoolagenda heeft een formaat van 30.3 x 21.5 x 1.5 cm. Deze agenda 2018-2019 loopt van 23 juli 2018 tot 28 juli 2019 met één week verdeeld over twee pagina's. Lesrooster, cijferlijst en plaats voor notities ontbreken niet in deze super school-agenda. Een weekoverzicht over twee pagina’s en door de gebonden rug blijft de organizer goed open liggen. Bekijk ook onze schoolcampus 18-19 voor al je schoolspullen.
+                        {product.description}
                     </div>
                 </div>
             </div>
