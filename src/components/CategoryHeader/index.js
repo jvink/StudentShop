b21edbaf44df869303c7a961bcfb8abb8a9a220c
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2018-10-30 18:58:22 +0100

    categories and subcategories now work

--- src/components/CategoryHeader/index.js
+++ src/components/CategoryHeader/index.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import '../../styles/header.css';
+var categories = require('../../categories.json');
 
 class CategoryHeader extends Component {
     render() {
-        const categories = [
-            {categoryName: "Agenda's", categoryLink: '/agendas'},
-            {categoryName: "Boeken", categoryLink: '/boeken'},
-            {categoryName: "Schoolspullen", categoryLink: '/schoolspullen'},
-            {categoryName: "Tassen", categoryLink: '/tassen'},
-            {categoryName: "Kleding", categoryLink: '/kleding'},
-            {categoryName: "Laptops en accessoires", categoryLink: '/laptops'}
-        ];
-        
         return (
-        <div className="wrapperSubHeader">
-            <div className="subHeaderItemsWrapper">
-                {categories.map((categorie) => {
-                    return (
-                        <div className="categoryWrapper">
-                            <div className="category">{categorie.categoryName}</div>
-                            <div className="categoryDivider">|</div>
-                        </div>
-                    );
-                })}
+            <div className="wrapperSubHeader">
+                <div className="subHeaderItemsWrapper">
+                    {categories.map((category) => {
+                        return (
+                            <div class="dropdown">
+                                <button class="dropbtn">
+                                    <div className="categoryName">{category.categoryName}</div>
+                                </button>
+                                <div class="dropdown-content">
+                                    {category.subCategories.map((subCategory) => {
+                                        return (
+                                            <a href={category.categoryLink + subCategory.subCategoryLink}>{subCategory.subCategoryName}</a>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-        </div>
         );
     }
 }
