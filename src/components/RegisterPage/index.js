ac17abd3ef874c1e6974c6c02f35ca4a34de5a4b
Joessi Moorman <33626659+JoessiM@users.noreply.github.com>
2018-11-13 15:17:24 +0100

    registerpage update

--- src/components/RegisterPage/index.js
+++ src/components/RegisterPage/index.js
@@ -3,40 +3,101 @@ import React, { Component } from 'react';
 
 class RegisterPage extends Component {
     constructor(props) {
-        super(props);
-        this.state = {naam: '', adres: '', postcode: ''};
+    super(props);
+    this.state = {
+    naam: "",
+        straatnaam: "",
+        postcode: "",
+        stad: "",
+        huisnummer: "",
+        toevoeging: ""
+
+};
 
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+        [name]: value
+    });
+    }
+handleSubmit(event) {
+      alert(this.state.value);
+      event.preventDefault();
     }
 
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    render() {
-        return (
-            <form>
+render() {
+    return (
+        <form>
+            <label>
+                Hr./Mvr.:
+          <select value={this.state.value} >
+                    <option value="Hr.">Hr.</option>
+                    <option value="Mvr.">Mvr.</option>
+                    onChange={this.handleInputChange}
+                </select>
+            </label>
+            <br />
+            <label>
+                Naam:
+          <input
+                    name="naam"
+                    type="text"
+                    value={this.state.naam}
+                    onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <label>
+                Straatnaam:
+          <input
+                    name="straatnaam"
+                    type="text"
+                    value={this.state.straatnaam}
+                    onChange={this.handleInputChange} />
+            </label>
                 <label>
-                    Naam:
-          <input type="text" value={this.state.naam} onChange={this.handleInputChange} />
-                </label>
-                <label>
-                    Adres:
-          <input type="text" value={this.state.adres} onChange={this.handleInputChange} />
-                </label>
-                <label>
-                    Postcode:
-          <input type="text" value={this.state.postcode} onChange={this.handleInputChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-    }
+                    Huisnummer:
+          <input
+                        name="huisnummer"
+                        type="number"
+                        value={this.state.huisnummer}
+                        onChange={this.handleInputChange} />
+            </label>
+            <label>
+                Toevoeging:
+          <input
+                    name="toevoeging"
+                    type="text"
+                    value={this.state.toevoeging}
+                    onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <label>
+                Postcode:
+          <input
+                    name="postcode"
+                    type="text"
+                    value={this.state.postcode}
+                    onChange={this.handleInputChange} />
+            </label>
+            <label>
+                Stad:
+          <input
+                    name="stad"
+                    type="text"
+                    value={this.state.stad}
+                    onChange={this.handleInputChange} />
+            </label>
+            <input type="submit" value="submit" />
+        </form>
+    );
 }
+}
+
 export default RegisterPage;
