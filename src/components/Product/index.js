9c4fc0b7fbee1b1e6fec1639e39a4ea9fb65fce6
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2018-11-19 16:22:53 +0100

    visual feedback on clicking the favourite button

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -13,25 +13,53 @@ const toastrOptions = {
 };
 
 class Product extends Component {
-    state = {
-        isFavorite: false
-    };
+    constructor(props) {
+        super(props);
 
-    onChange(productId) {
-        let { isFavorite } = this.state;
-        this.setState({isFavorite: !isFavorite});
-        isFavorite ? toastr.light('Uit favorieten verwijderd', toastrOptions) : toastr.light('Aan favorieten toegevoegd', toastrOptions);
+        this.state = {
+            favs: []
+        }
     }
-    
+
+    componentWillMount() {
+        const { data } = this.props;
+        data.map((product) => {
+            return this.setState(prevState => ({
+                favs: [...prevState.favs, {productId: product.id, isFav: product.isFavourite}]
+            }));
+        });
+    }
+
+    onChange(productId, isFavourite) {
+        for (let i = 0; i < this.state.favs.length; i++) {
+            if (this.state.favs[i].productId === productId) {
+                var stateCopy = Object.assign({}, this.state);
+                stateCopy.favs = stateCopy.favs.slice();
+                stateCopy.favs[i] = Object.assign({}, stateCopy.favs[i]);
+                stateCopy.favs[i].isFav = !stateCopy.favs[i].isFav;
+                this.setState(stateCopy);
+            }
+        }
+        this.props.addToFavourites(productId);
+        isFavourite ? toastr.light('Uit favorieten verwijderd', toastrOptions) : toastr.light('Aan favorieten toegevoegd', toastrOptions);
+    }
+
+    isFavourite(productId) {
+        for (let i = 0; i < this.state.favs.length; i++) {
+            if (this.state.favs[i].productId === productId) {
+                return this.state.favs[i].isFav;
+            }
+        }
+    }
+
     render() {
         const { data } = this.props;
-        
         return data.map((product) => {
             return (
                 <div className="productWrapper" key={product.id}>
                     <PriceLabel price={product.price}/>
-                    <div className="productFavorite" onClick={() => {this.props.addToFavourites(product.id)}}>
-                        {this.state.isFavorite ? <Favorite className="isFavorite"/> : <FavoriteBorder/>}
+                    <div className="productFavorite" onClick={() => {this.onChange(product.id, this.isFavourite(product.id))}}>
+                        {this.isFavourite(product.id) ? <Favorite className="isFavorite"/> : <FavoriteBorder/>}
                     </div>
                     <Link to={"product/" + product.id} className="productLink">
                         {product.firstImg ? <img src={product.firstImg} alt={product.id} className="productImage"/> : <img src="https://raw.githubusercontent.com/jvink/project-c/master/src/images/no-image.jpg?token=AafImDyyZnKhuduvH2v0ac9GcDX5zhBhks5b8_FnwA%3D%3D" alt="NotFound" className="productItemImage"/>}
