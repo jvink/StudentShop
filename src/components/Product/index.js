23e72f448ac18354667f1e338a0322d980116cb5
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2018-11-19 18:03:09 +0100

    changed structure again to flexbox

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -62,7 +62,7 @@ class Product extends Component {
                         {this.isFavourite(product.id) ? <Favorite className="isFavorite"/> : <FavoriteBorder/>}
                     </div>
                     <Link to={"product/" + product.id} className="productLink">
-                        {product.firstImg ? <img src={product.firstImg} alt={product.id} className="productImage"/> : <img src="https://raw.githubusercontent.com/jvink/project-c/master/src/images/no-image.jpg?token=AafImDyyZnKhuduvH2v0ac9GcDX5zhBhks5b8_FnwA%3D%3D" alt="NotFound" className="productItemImage"/>}
+                        {product.firstImg ? <img src={product.firstImg} alt={product.id} className="productImage"/> : <img src="https://raw.githubusercontent.com/jvink/project-c/master/src/images/no-image.jpg?token=AafImDyyZnKhuduvH2v0ac9GcDX5zhBhks5b8_FnwA%3D%3D" alt="NotFound" className="productImage"/>}
                         <ProductNameLabel name={product.name}/>
                     </Link>
                 </div>
