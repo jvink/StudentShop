31c04d3898bd0d9a2cf47e5613bbf080b5b5322c
Jurian-PC\Jurian <j.vink@gdksoftware.nl>
2019-01-11 02:54:20 +0100

    added ispayed to shopping cart container

--- src/containers/ShoppingCart/index.js
+++ src/containers/ShoppingCart/index.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import cartActionCreator from '../../store/actionCreators/cart';
 import ShoppingCart from '../../components/ShoppingCart';
+import OrderDone from '../../components/OrderDone';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import '../../styles/product.css';
 
 class ShoppingCartContainer extends Component {
@@ -23,13 +25,23 @@ class ShoppingCartContainer extends Component {
         this.props.cartActions.deleteAllFromCart(this.props.token);
     }
 
+    payProductsFromCart() {
+        this.props.cartActions.payProductsFromCart(this.props.token);
+    }
+
     render() {
-        if (this.props.cartStore.isGettingCart) {
+        if (this.props.cartStore.isPayingCart) {
+            return  <div style={{width: '100%', textAlign: 'center', margin: '2em'}}><CircularProgress/><h4>Bestelling aan het afronden...</h4></div>
+        } else if (this.props.cartStore.payCartError) {
+            return <p>Er is iets misgegaan met uw bestelling</p>
+        } else if (this.props.cartStore.payCartResult) {
+            return <OrderDone/>
+        } else if (this.props.cartStore.isGettingCart) {
             return <p>Loading..</p>
         } else if (this.props.cartStore.getCartResult) {
             return (
                 <div className="productsWrapper">
-                    <ShoppingCart removeAllFromCart={() => this.removeAllFromCart()} removeFromCart={(productId) => this.removeFromCart(productId)} data={this.props.cartStore.getCartResult}/>
+                    <ShoppingCart payProductsFromCart={() => this.payProductsFromCart()} removeAllFromCart={() => this.removeAllFromCart()} removeFromCart={(productId) => this.removeFromCart(productId)} data={this.props.cartStore.getCartResult}/>
                 </div>
             );
         } else if (this.props.cartStore.getCartError) {
